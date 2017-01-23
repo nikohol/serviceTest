50786cfbe2a542667ae07dde4f7af94ec35e069b
nikohol <nikohol@metropolia.fi>
2017-01-23 14:11:04 +0200

    service works

--- src/app/services/media.service.ts
+++ src/app/services/media.service.ts
@@ -1,24 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { Component, OnInit } from '@angular/core';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class MediaService {
 
-  private mod: any = {};
-
   constructor(private http: Http) { }
 
-  getJson() {
-    this.http.get('tsconfig.json')
-    .subscribe((res: Response) => {
-      const json = res.json();
-      this.mod = json.compilerOptions;
-      console.log(this.mod);
-    });
-  }
   getAllMedia() {
-    return null;
+    this.http.get('http://media.mw.metropolia.fi/wbma/media/')
+    .map((resp) => {
+      return resp.json();
+    });
   }
 
 }
